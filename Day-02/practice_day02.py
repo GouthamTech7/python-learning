eb7294e8831c658cd1be4149550cd3581142ee16
Goutham <jaigansh123@gmail.com>
2026-09-07 20:19:18 +0530

    Organize Day 1 and add Day 2 Python programs

---
+++ Day-02/practice_day02.py
@@ -0,0 +1,17 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+addition =number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+
+print("\n--- Student Caluclator ---")
+print("Name:", name)
+print("Age: ", age)
+print("Addition: ", addition)
+print("Subtraction: ", subtraction)
+print("Multiplication: ", multiplication)
+print("Division: ",division)
