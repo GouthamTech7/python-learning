89f01db21ef67c1bc2aef67701eff27ccb8b83b3
Goutham <jaigansh123@gmail.com>
2026-09-07 20:55:23 +0530

    Add Day 3 Python Decision making programs

---
+++ Day-03/eligibility_checker.py
@@ -0,0 +1,8 @@
+name = input("ENter your name: ")
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print(f"Hello {name}, you are eligible")
+else:
+    print(f"Hello {name}, you are not eligible")
+    
