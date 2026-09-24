82762d27b2fb1bd21f1de77fede8f3d46c824736
Goutham <jaigansh123@gmail.com>
2026-09-24 20:24:43 +0530

    01-Python-Fundamentals/Practice-Programs/02-average-calculator.py

---
+++ 01-Python-Fundamentals/Practice-Programs/02-average-calculator.py
@@ -0,0 +1,14 @@
+name = input("Enter your name: ")
+
+maths = float(input("Enter your Maths marks: "))
+physics = float(input("Enter your Physics marks: "))
+english = float(input("Enter your English marks: "))
+
+total_marks = maths + physics + english
+average_marks = total_marks / 3
+
+print("Student Name:", name)
+print("Total Marks:", total_marks)
+print("Average Marks:", average_marks)
+
+
