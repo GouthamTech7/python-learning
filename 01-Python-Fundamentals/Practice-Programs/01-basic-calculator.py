ff5608f18380314562e0ae31626bbe031d90da83
Goutham <jaigansh123@gmail.com>
2026-09-24 20:17:08 +0530

    Add Python practice programs

---
+++ 01-Python-Fundamentals/Practice-Programs/01-basic-calculator.py
@@ -0,0 +1,10 @@
+print("Simple Calculator")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+ 
+print("1. Addition:", num1 + num2)
+print("2. Subtraction:", num1 - num2)
+print("3. Multiplication:", num1 * num2)
+print("4. Division:", num1 / num2)
