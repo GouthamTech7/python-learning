4fdf3deb5c99327c9ac29521f1bf375d47a34ebe
Goutham <jaigansh123@gmail.com>
2026-09-24 20:28:04 +0530

    01-Python-Fundamentals/Practice-Programs/3-product-calculator.py

---
+++ 01-Python-Fundamentals/Practice-Programs/3-product-calculator.py
@@ -0,0 +1,8 @@
+product_name = input("Enter the product name: ")
+price = float(input("Enter the price of the product: "))
+quantity = int(input("Enter the quantity of the product: "))
+
+total_cost = price * quantity
+
+print("Product Name:", product_name)
+print("Total Cost:", total_cost)
